3129fc0c34348457a6ffd7e3689ad70dcf28b439
kakaori <kaoneko6622@gmail.com>
2024-06-30 19:01:57 +0900

    post追加

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\GiftController;
+
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +17,14 @@ Route::get('/giftcard', [GiftController::class, 'create'])->name('giftcard.creat
 Route::post('/giftcard/preview', [GiftController::class, 'preview'])->name('giftcard.preview');
 //giftcard：追加完了・表示
 Route::post('/giftcard/preview/complete', [GiftController::class, 'store'])->name('giftcard.store');
+
+
+//post 入力画面
+Route::get('/post', [PostController::class, 'create'])->name('post.create');
+//post 登録処理
+Route::post('/post/complete', [PostController::class, 'store'])->name('post.store');
+
+
 
 
 Route::get('/dashboard', function () {
