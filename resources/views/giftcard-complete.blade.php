454241d9f09f13adedf2a62eb62a4ac117337674
TaikiHattori <tvajarvis@outlook.jp>
2024-06-30 15:15:19 +0900

    決済完了画面まで

--- resources/views/giftcard-complete.blade.php
+++ resources/views/giftcard-complete.blade.php
@@ -3,11 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/pagedone@1.1.2/src/css/pagedone.css"/>
     <title>Document</title>
 </head>
 <body>
 
-<h1>決済完了</h1>
+ <section class="py-24">
+      <div class="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+          <div class="w-full flex-col justify-start items-center gap-10 inline-flex">
+              <div class="w-full flex-col justify-start items-center gap-4 flex">
+                  <h2 class="w-full text-center text-black text-3xl font-bold font-manrope leading-normal">決済完了</h2>
+                  <p class="max-w-4xl text-center text-gray-500 text-base font-normal leading-relaxed">ご購入ありがとうございます。</p>
+              </div>
+              <div class="w-full max-w-screen-sm flex-col justify-start items-start gap-10 flex">
+                  <div class="w-full py-9 rounded-xl flex-col justify-start items-start flex">
+                      <div class="w-full flex-col justify-center sm:items-center items-start gap-8 flex">
+                          <ol class="flex sm:items-center items-start w-full sm:gap-0 gap-5">
+                              <li class="flex w-full relative justify-center text-orange-600 text-base font-semibold after:content-['']  after:w-full after:h-0.5 after:bg-orange-600 after:inline-block after:absolute lg:after:top-4 after:top-3 xl:after:left-24 lg:after:left-28 md:after:left-24 sm:after:left-32 after:left-20">
+                              <div class="block sm:whitespace-nowrap z-10 flex flex-col items-center text-center">
+                                  <span class="w-6 h-6 bg-orange-600 text-center border-2 border-transparent rounded-full flex justify-center items-center mx-auto mb-1 text-base font-bold text-white lg:w-8 lg:h-8">1</span> 作成
+                              </div>
+                              </li>
+                              <li class="flex w-full relative justify-center text-orange-600 text-base font-semibold after:content-['']  after:w-full after:h-0.5 after:bg-orange-600 after:inline-block after:absolute lg:after:top-4 after:top-3 xl:after:left-24 lg:after:left-28 md:after:left-24 sm:after:left-32 after:left-20">
+                              <div class="block sm:whitespace-nowrap z-10 flex flex-col items-center text-center">
+                                  <span class="w-6 h-6 bg-orange-600 text-center border-2 border-transparent rounded-full flex justify-center items-center mx-auto mb-1 text-base font-bold text-white lg:w-8 lg:h-8">2</span> プレビュー
+                              </div>
+                              </li>
+                              <li class="flex w-full relative justify-center text-orange-600 text-base font-semibold">
+                                  <div class="block sm:whitespace-nowrap z-10 flex flex-col items-center text-center">
+                                      <span class="w-6 h-6 bg-orange-600 text-center border-2 border-transparent rounded-full flex justify-center items-center mx-auto mb-1 text-base font-bold text-white lg:w-8 lg:h-8">3</span> 完了
+                                  </div>
+                              </li>
+                          </ol>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+
+          <div class="w-full mx-auto max-w-screen-md">
+            <p class="max-w-4xl text-center text-gray-500 text-base font-normal leading-relaxed">プレゼントの準備ができました。</p>
+
+            <div class="w-full max-w-md mt-5 text-center m-auto">
+              <img class="w-full lg:mx-0 mx-auto h-full" src="https://pagedone.io/asset/uploads/1701157806.png" alt="about Us image" />
+            </div>
+
+
+              <button class="mx-auto mt-10 sm:w-fit w-full px-9 py-3 bg-orange-600 hover:bg-orange-700 ease-in-out transition-all duration-700 rounded-xl shadow justify-center items-center flex">
+                  <span class="px-3.5 text-center text-white text-lg font-semibold leading-8">受け取りリンクをコピーする</span>
+              </button>
+          </div>
+      </div>
+  </section>
+
+ <script src="https://cdn.jsdelivr.net/npm/pagedone@1.1.2/src/js/pagedone.js"></script>
+
 </body>
 </html>
