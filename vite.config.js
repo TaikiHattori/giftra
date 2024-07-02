f08d79c22fad8af86177ccb632208f19f4c1e69e
kakaori <kaoneko6622@gmail.com>
2024-07-02 11:32:02 +0900

    photo画面の修正

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
                 'resources/assets/modulepreload-polyfill.b7f2da20.js',
                 'resources/assets/vendor.602a9696.js',
                 'resources/assets/effect-tinder.f396dd4b.js',
+                'resources/assets/effect-tinder.aef0cf08.css',
+                'resources/assets/tinder.17df37a4.css',
             ],
             refresh: true,
         }),
