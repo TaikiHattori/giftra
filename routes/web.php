0e15fc94855036672c300f90a0f25dad78a3ff10
kakaori <kaoneko6622@gmail.com>
2024-07-03 23:00:45 +0900

    dashboardの表示を変更

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::post('/guide', [PostController::class, 'show'])->name('guide.show');
 
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('welcome');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
