65accfed44580adfff57ae71a84408fcad85ee0e
kakaori <kaoneko6622@gmail.com>
2024-07-04 01:24:26 +0900

    footerにマイページ追加

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,29 @@
-    <!-- footer -->
-    <footer class="w-full ">
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div class="py-7 border-t border-gray-200">
-            <div class="flex items-center justify-center">
-              <span class="text-gray-400 ">© giftra 2024, All rights reserved.</span>
-            </div>
-          </div>
-        </div>
-      </footer>
-    <!-- end footer -->
+<!-- footer -->
+<footer class="w-full ">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 
-    <script src="https://cdn.jsdelivr.net/npm/pagedone@1.1.2/src/js/pagedone.js"></script>
+    <!--Grid-->
+    <div class="py-5 flex justify-between items-center flex-col gap-8 xl:flex-row">
+      <div class="flex items-center flex-col xl:flex-row ">
+        <ul class="text-lg  flex items-center flex-col md:flex-row  py-2 gap-6 md:gap-12 xl:border-l border-gray-200 xl:ml-11 xl:pl-11 transition-all duration-500">
+          <li>
+            <a href="/profile" class="text-gray-400 hover:text-orange-600">マイページ</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--Grid-->
+
+
+    <div class="py-7 border-t border-gray-200">
+      <div class="flex items-center justify-center">
+        <span class="text-gray-400 ">© giftra 2024, All rights reserved.</span>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- end footer -->
+
+<script src="https://cdn.jsdelivr.net/npm/pagedone@1.1.2/src/js/pagedone.js"></script>
 </body>
 </html>
